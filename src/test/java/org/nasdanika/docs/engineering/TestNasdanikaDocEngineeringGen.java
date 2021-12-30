fa2d3ec77df740447ac6bf7c6712dc3948df062e
Pavel <Pavel.Vlasov@nasdanika.org>
2021-12-30 08:41:22 -0500

    End-to-end generation - model doc, engineering doc.

--- src/test/java/org/nasdanika/docs/engineering/TestNasdanikaDocEngineeringGen.java
+++ src/test/java/org/nasdanika/docs/engineering/TestNasdanikaDocEngineeringGen.java
@@ -4,20 +4,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
+import org.apache.commons.codec.binary.Hex;
+import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
+import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -27,7 +39,10 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.junit.Test;
 import org.nasdanika.common.Context;
+import org.nasdanika.common.DefaultConverter;
 import org.nasdanika.common.Diagnostic;
+import org.nasdanika.common.DiagramGenerator;
+import org.nasdanika.common.MutableContext;
 import org.nasdanika.common.NasdanikaException;
 import org.nasdanika.common.NullProgressMonitor;
 import org.nasdanika.common.ProgressMonitor;
@@ -47,14 +62,18 @@ import org.nasdanika.exec.resources.ReconcileAction;
 import org.nasdanika.exec.resources.ResourcesFactory;
 import org.nasdanika.exec.resources.ResourcesPackage;
 import org.nasdanika.flow.FlowPackage;
+import org.nasdanika.html.ecore.EcoreActionSupplierAdapterFactory;
+import org.nasdanika.html.ecore.GenModelResourceSet;
 import org.nasdanika.html.emf.EObjectActionResolver;
 import org.nasdanika.html.model.app.Action;
 import org.nasdanika.html.model.app.AppPackage;
 import org.nasdanika.html.model.app.gen.AppAdapterFactory;
 import org.nasdanika.html.model.app.gen.Util;
 import org.nasdanika.html.model.app.util.ActionProvider;
+import org.nasdanika.html.model.app.util.ActionSupplier;
 import org.nasdanika.html.model.bootstrap.BootstrapPackage;
 import org.nasdanika.html.model.html.HtmlPackage;
+import org.nasdanika.ncore.Marker;
 import org.nasdanika.ncore.NcorePackage;
 import org.nasdanika.ncore.util.NcoreResourceSet;
 
@@ -99,6 +118,31 @@ public class TestNasdanikaDocEngineeringGen extends TestBase {
 					assertThat(severity).isEqualTo(org.eclipse.emf.common.util.Diagnostic.OK);
 										
 					try {
+						// Marker - TODO - use jGit because of a federated nature of the engineering model 
+						Map<String,String> originMap = new HashMap<>();
+						originMap.put(new File(".").getCanonicalFile().toURI().toString(), "https://github.com/Nasdanika/nasdanika.github.io/blob/main/");
+						originMap.put(new File("../core").getCanonicalFile().toURI().toString(), "https://github.com/Nasdanika/core/blob/master/");
+						originMap.put(new File("../html").getCanonicalFile().toURI().toString(), "https://github.com/Nasdanika/html/blob/master/");
+						originMap.put(new File("../engineering").getCanonicalFile().toURI().toString(), "https://github.com/Nasdanika/engineering/blob/main/");
+						originMap.put(new File("../togaf").getCanonicalFile().toURI().toString(), "https://github.com/Nasdanika/togaf/blob/main/");
+						TreeIterator<EObject> cit = copy.eAllContents(); 
+						while (cit.hasNext()) {
+							EObject next = cit.next(); 
+							if (next instanceof Marker) {
+								Marker marker = (Marker) next;
+								String location = marker.getLocation();
+								for (Map.Entry<String,String> originEntry: originMap.entrySet()) {
+									if (location != null && location.startsWith(originEntry.getKey())) {
+										marker.setLocation(location.substring(originEntry.getKey().length()));
+										marker.setOrigin(originEntry.getValue() + marker.getLocation());
+										if (marker.getLine() > 0) {
+											marker.setOrigin(marker.getOrigin() + "#L" + marker.getLine());
+										}
+									}
+								}
+							}
+						}
+						
 						instanceModelResource.save(null);
 					} catch (IOException ioe) {
 						throw new NasdanikaException(ioe);
@@ -116,6 +160,139 @@ public class TestNasdanikaDocEngineeringGen extends TestBase {
 				progressMonitor);		
 	}
 	
+	private DiagramGenerator createDiagramGenerator(ProgressMonitor progressMonitor) {
+		FileSystemContainer output = new FileSystemContainer(new File("target\\diagram-cache"));
+		
+		BiFunction<String,InputStream,String> decoder = (path, state) -> DefaultConverter.INSTANCE.convert(state, String.class);
+		BiFunction<String,String,InputStream> encoder = (path, state) -> DefaultConverter.INSTANCE.convert(state, InputStream.class);
+		return DiagramGenerator.INSTANCE.cachingDiagramGenerator(output.stateAdapter().adapt(decoder, encoder), progressMonitor);
+	}
+	
+	private boolean isSameURI(EPackage a, EPackage b) {
+		return a !=  null && b != null && a.getNsURI().equals(b.getNsURI());
+	}
+		
+	public void generateEcoreActionModel(ProgressMonitor progressMonitor) throws Exception {
+		GenModelResourceSet ecoreModelsResourceSet = new GenModelResourceSet();
+		
+		Map<String,String> pathMap = new ConcurrentHashMap<>();
+		
+		Function<EPackage,String> getEPackagePath = ePackage -> {
+			if (isSameURI(ePackage, NcorePackage.eINSTANCE)) {
+				return "core/modules/ncore";
+			}
+			if (isSameURI(ePackage, ExecPackage.eINSTANCE)) {
+				return "core/modules/exec/modules/model";
+			}
+			if (isSameURI(ePackage, FlowPackage.eINSTANCE)) {
+				return "core/modules/flow";
+			}
+			if (isSameURI(ePackage, DiagramPackage.eINSTANCE)) {
+				return "core/modules/diagram/modules/model";
+			}
+
+			if (isSameURI(ePackage, AppPackage.eINSTANCE)) {
+				return "html/modules/models/modules/app/modules/model";
+			}
+			if (isSameURI(ePackage, BootstrapPackage.eINSTANCE)) {
+				return "html/modules/models/modules/bootstrap/modules/model";
+			}
+			if (isSameURI(ePackage, HtmlPackage.eINSTANCE)) {
+				return "html/modules/models/modules/html/modules/model";
+			}
+
+			if (isSameURI(ePackage, EngineeringPackage.eINSTANCE)) {
+				return "engineering/modules/model";
+			}
+			
+			for (int i = 0; i < Integer.MAX_VALUE; ++i) {
+				String path = i == 0 ? ePackage.getName() : ePackage.getName() + "_" + i;
+				if (pathMap.containsKey(path)) {
+					if (ePackage.getNsURI().equals(pathMap.get(path))) {
+						return path;
+					}
+				} else {
+					pathMap.put(path, ePackage.getNsURI());
+					return path;
+				}
+			}
+			
+			// Encoding NS URI as HEX. Shall never reach this point.
+			return Hex.encodeHexString(ePackage.getNsURI().getBytes(StandardCharsets.UTF_8));
+		};
+		
+		MutableContext context = Context.EMPTY_CONTEXT.fork();
+		
+		DiagramGenerator diagramGenerator = createDiagramGenerator(progressMonitor);
+		context.register(DiagramGenerator.class, diagramGenerator);//DiagramGenerator.createClient(new URL("http://localhost:8090/spring-exec/api/v1/exec/diagram/")));
+		
+		ecoreModelsResourceSet.getAdapterFactories().add(new EcoreActionSupplierAdapterFactory(context, getEPackagePath));
+		
+		// Physical location relative to the projects (git) root folder -> logical (workspace) name 
+		Map<String,String> bundleMap = new LinkedHashMap<>();
+		
+		bundleMap.put("core/diagram", "org.nasdanika.diagram");
+		bundleMap.put("core/exec", "org.nasdanika.exec");
+		bundleMap.put("core/flow", "org.nasdanika.flow");
+		bundleMap.put("core/ncore", "org.nasdanika.ncore");
+				
+		bundleMap.put("html/model/app", "org.nasdanika.html.model.app");
+		bundleMap.put("html/model/bootstrap", "org.nasdanika.html.model.bootstrap");
+		bundleMap.put("html/model/html", "org.nasdanika.html.model.html");
+		
+		bundleMap.put("engineering/model", "org.nasdanika.engineering");
+	
+		File modelDir = new File("target/models").getAbsoluteFile();
+		modelDir.mkdirs();
+		
+		File modelDocActionsDir = new File("target/model-doc/actions").getAbsoluteFile();
+		delete(modelDocActionsDir);
+		modelDocActionsDir.mkdirs();
+		
+		Map<URI,File> modelToActionModelMap = new LinkedHashMap<>();
+		
+		File projectsRoot = new File("..");		
+		for (Entry<String, String> be: bundleMap.entrySet()) {					
+			File sourceDir = new File(projectsRoot, be.getKey());
+			File targetDir = new File(modelDir, be.getValue());
+			copy(new File(sourceDir, "model"), new File(targetDir, "model"), true, (source, target) -> {
+				if (target.getName().endsWith(".genmodel")) {
+					modelToActionModelMap.put(URI.createFileURI(target.getAbsolutePath()), new File(modelDocActionsDir, target.getName() + ".xml"));
+				}
+			});			
+			copy(new File(sourceDir, "doc"), new File(targetDir, "doc"), true, null);
+		}		
+		
+		// Loading resources to the resource set.
+		for (URI uri: modelToActionModelMap.keySet()) {
+			ecoreModelsResourceSet.getResource(uri, true);
+		}		
+		
+		EcoreUtil.resolveAll(ecoreModelsResourceSet);
+		
+		ResourceSet actionModelsResourceSet = new ResourceSetImpl();
+		actionModelsResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
+		
+		// Generating
+		for (URI uri: modelToActionModelMap.keySet()) {
+			Resource ecoreModelResource = ecoreModelsResourceSet.getResource(uri, false);
+			File output = modelToActionModelMap.get(ecoreModelResource.getURI());
+			
+			Resource actionModelResource = actionModelsResourceSet.createResource(URI.createFileURI(output.getAbsolutePath()));
+			
+			for (EObject contents: ecoreModelResource.getContents()) {
+				if (contents instanceof GenModel) {
+					for (GenPackage genPackage: ((GenModel) contents).getGenPackages()) {
+						EPackage ecorePackage = genPackage.getEcorePackage();
+						actionModelResource.getContents().add(EObjectAdaptable.adaptTo(ecorePackage, ActionSupplier.class).execute(progressMonitor));
+					}
+				}
+			}
+	
+			actionModelResource.save(null);
+		}		
+	}
+		
 	/**
 	 * Loads instance model from previously generated XMI, diagnoses, generates action model.
 	 * @throws Exception
@@ -297,17 +474,23 @@ public class TestNasdanikaDocEngineeringGen extends TestBase {
 		ACTION_MODELS_DIR.mkdirs();
 		RESOURCE_MODELS_DIR.mkdirs();
 		
-		generateSite("nasdanika");
+		ProgressMonitor progressMonitor = new NullProgressMonitor(); // PrintStreamProgressMonitor();
+		
+		long start = System.currentTimeMillis();
+		generateEcoreActionModel(progressMonitor);
+		System.out.println("\tGenerated ecore action model in " + (System.currentTimeMillis() - start) + " milliseconds");
+		start = System.currentTimeMillis();
+		
+		generateSite("nasdanika", progressMonitor);
 
 		long cacheMisses = FeatureCacheAdapter.getMisses();
 		long cacheCalls = FeatureCacheAdapter.getCalls();
 		long cacheEfficiency = 100*(cacheCalls - cacheMisses)/cacheCalls;
-		System.out.println("Feature cache - calls: " + cacheCalls + ", misses: " + cacheMisses + ", efficiency: " + cacheEfficiency + "%, compute time: " + FeatureCacheAdapter.getComputeTime() + " nanoseconds.");
+		System.out.println("Feature cache - calls: " + cacheCalls + ", misses: " + cacheMisses + ", efficiency: " + cacheEfficiency + "%, compute time: " + TimeUnit.NANOSECONDS.toMillis(FeatureCacheAdapter.getComputeTime()) + " milliseconds.");
 	}
 	
-	private void generateSite(String name) throws Exception {
+	private void generateSite(String name, ProgressMonitor progressMonitor) throws Exception {
 		System.out.println("Generating site: " + name);
-		ProgressMonitor progressMonitor = new NullProgressMonitor(); // PrintStreamProgressMonitor();
 		
 		long start = System.currentTimeMillis();
 		generateEngineeringModel(name, progressMonitor);
